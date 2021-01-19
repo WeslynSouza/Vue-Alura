47f7ccc3ab59680d0f42cdd9e3054409e3376c15
weslynSouza <wesley.i476@gmail.com>
2021-01-19 20:29:47 -0300

    criando components

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,61 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+	<div class='grupo'>
+		<h1 class='centralizado'>{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo">
-      </li>
-    </ul>
-
-  </div>
+		<ul class='lista-fotos'>
+			<li class='lista-fotos-item' v-for="foto in fotos">
+				<meu-painel :titulo="foto.titulo">
+					<img class='imagem-responsiva' :src="foto.url" :alt="foto.titulo">
+				</meu-painel>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-export default {
-  
-  data() {
+	import Painel from './components/shared/painel/Painel.vue';
 
-    return {
-      titulo: "Alurapic",
-      fotos: [],
-    }
-  },
+	export default {
 
-  created() {
-   
-   this.$http.get('http://localhost:3000/v1/fotos')
-    .then(res => res.json())
-    .then(fotos => this.fotos = fotos, err => console.log(err));
-  }
-}
+		components: {
+			'meu-painel': Painel
+		},
+
+		data() {
+			return {
+				titulo: "Alurapic",
+				fotos: [],
+			}
+		},
+		created() {
+			this.$http.get('http://localhost:3000/v1/fotos')
+				.then(res => res.json())
+				.then(fotos => this.fotos = fotos, err => console.log(err));
+		}
+	}
 </script>
 
 <style>
+	.centralizado {
+		text-align: center;
+	}
+
+	.corpo {
+		font-family: Helvetica, sans-serif;
+		margin: 0 auto;
+		width: 96%;
+	}
+
+	.lista-fotos {
+		list-style: none;
+	}
+
+	.lista-fotos .lista-fotos-item {
+		display: inline-block;
+	}
+
+	.imagem-responsiva { 
+		width: 100%;
+	}
 
 </style>
